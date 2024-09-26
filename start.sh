8db5ce260557a4b8dbedaa49384e4a221d649d5c
NiktowCode <1496304@uab.cat>
2024-09-26 13:41:52 +0200

    More connection problems

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ docker build -t bitcoin .
 
 
 # Port forwarding to connect from external source to our private network
-docker run -it -d --network bitcoin_network --name bc-node1 -p 8332:8332 bitcoin -printtoconsole -txindex=1 -reindex
+docker run -it -d --network bitcoin_network --name bc-node1 -p 8333:8333 bitcoin -printtoconsole -txindex=1 -reindex
 docker run -it -d --network bitcoin_network --name bc-node2 bitcoin -printtoconsole -txindex=1 -reindex
 docker run -it -d --network bitcoin_network --name bc-node3 bitcoin -printtoconsole -txindex=1 -reindex
